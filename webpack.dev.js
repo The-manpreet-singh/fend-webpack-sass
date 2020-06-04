0892ad747cf00cf36b28fa36ebe98ec9f3a77d86
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-04 13:47:50 +0530

    add css pro

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,13 +5,13 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
     output: {
         libraryTarget: 'var',
         library: 'Client'
     },
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
     module: {
         rules: [
             {
